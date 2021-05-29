761f0ae78679d0a8bfeb44f55fd43305c61edb93
Giri Dhar <giridharreddy1000@gmail.com>
2021-05-29 11:53:58 +0530

    Adding Files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Ram",
+    username: "Ram Shetty",
+    email: "ram@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Shyam",
+    username: "Shyam Roy",
+    email: "shyam23@gmail.com",
+  },
+  {
+    id: 3,
+    name: "John",
+    username: "John John",
+    email: "john@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Bob",
+    username: "Bob Pop",
+    email: "bob32@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
+];
